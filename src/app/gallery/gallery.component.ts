a5bb870c1c22ced76aa38de165f7b368290f01a0
Sudhan <s.sudhan@hyperthings.in>
2017-12-27 16:40:32 +0530

    add new gallery component

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-gallery',
@@ -10,6 +11,41 @@ export class GalleryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	var popup = {
+  init: function(){
+    $('figure').click(function(){
+      popup.open($(this));
+    });
+    
+    $(document).on('click', '.popup img', function(){
+      return false;
+    }).on('click', '.popup', function(){
+      popup.close();
+    })
+  },
+  open: function($figure) {
+    $('.gallery').addClass('pop');
+    let $popup = $('<div class="popup" />').appendTo($('body'));
+    let $fig = $figure.clone().appendTo($('.popup'));
+    let $bg = $('<div class="bg" />').appendTo($('.popup'));
+    let $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
+    let $shadow = $('<div class="shadow" />').appendTo($fig);
+    let src = $('img', $fig).attr('src');
+    $shadow.css({backgroundImage: 'url(' + src + ')'});
+    $bg.css({backgroundImage: 'url(' + src + ')'});
+    setTimeout(function(){
+      $('.popup').addClass('pop');
+    }, 10);
+  },
+  close: function(){
+    $('.gallery, .popup').removeClass('pop');
+    setTimeout(function(){
+      $('.popup').remove()
+    }, 100);
+  }
+}
+
+popup.init()
   }
 
 }
